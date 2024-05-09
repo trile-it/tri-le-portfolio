209c88ed401b609c59a4ffaa75dc60516de2b317
Tri Le <trile95.it@gmail.com>
2024-05-09 10:12:08 -0400

    add framer motion lib to animate the header appearance

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,27 @@
+"use client"
+
+import { motion } from "framer-motion";
 import React from 'react';
 
 function Header() {
-    return (
-        <header className="z-[999] relative">
-            <div className="
-                fixed top-0 left-1/2 -translate-x-1/2
-                h-[4.5rem] w-full
-                rounded-none
-                border border-white/40
-                bg-white/80
-                shadow-lg shadow-black/[0.03]
-                backdrop-blur-[0.5rem]
-                sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full
-            "></div>
-        </header>
-    );
+  return (
+    <header className="z-[999] relative">
+      <motion.div
+        className="
+          fixed top-0 left-1/2
+          h-[4.5rem] w-full
+          rounded-none
+          border border-white/40
+          bg-white/80
+          shadow-lg shadow-black/[0.03]
+          backdrop-blur-[0.5rem]
+          sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full
+        "
+        initial={{ x: "-50%", y: -100, opacity: 0 }}
+        animate={{ x: "-50%", y: 0, opacity: 1 }}
+      ></motion.div>
+    </header>
+  );
 }
 
 export default Header;
