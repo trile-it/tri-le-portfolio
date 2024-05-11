62e14bae77ca53c0f3ca633bc6aeac14807b5ce8
Tri Le <trile95.it@gmail.com>
2024-05-11 10:10:20 -0400

    add header links

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { links } from "@/lib/data";
 import { motion } from "framer-motion";
+import Link from "next/link";
 import React from 'react';
 
 function Header() {
@@ -20,6 +22,48 @@ function Header() {
         initial={{ x: "-50%", y: -100, opacity: 0 }}
         animate={{ x: "-50%", y: 0, opacity: 1 }}
       ></motion.div>
+
+      <nav
+        className="
+          flex
+          fixed top-[0.15rem] left-1/2 -translate-x-1/2
+          h-12
+          py-2
+          sm:top-[1.7rem] sm:h-[initial] sm:py-0
+        ">
+        <ul
+          className="
+            flex flex-wrap justify-center items-center gap-y-1
+            w-[22rem]
+            text-[0.9rem] font-medium text-gray-500
+            sm:w-[initial] sm:flex-nowrap sm:gap-5
+          "
+        >
+          {links.map(link => (
+            <motion.li
+              className="
+                h-3/4
+                flex justify-center items-center
+              "
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className="
+                  w-full
+                  flex justify-center items-center
+                  px-3 py-3
+                  hover:text-gray-950 transition
+                "
+                href={link.hash}
+              >
+                {link.name}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
