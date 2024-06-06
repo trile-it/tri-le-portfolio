3d205b737362a7a4a4993827de85c861cf8f226d
Tri Le <trile95.it@gmail.com>
2024-06-06 10:45:33 -0400

    add about section

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -42,7 +42,7 @@ function Intro() {
           </motion.span>
         </div>
       </div>
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -52,7 +52,7 @@ function Intro() {
         <span className="font-bold">3 years</span> of experience. I enjoy building{" "}
         <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="
