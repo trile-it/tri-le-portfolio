33e045f369f1eccd225d081c1660800af1615f04
Tri Le <trile95.it@gmail.com>
2024-06-08 11:20:09 -0400

    update intro info

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -62,7 +62,7 @@ function Intro() {
       >
         <span className="font-bold">Hello, I'm Tri Le (Trey).</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">3 years</span> of experience. I enjoy building{" "}
+        <span className="font-bold">3+ years</span> of experience. I enjoy building{" "}
         <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
