21c4061877a91b77ea2421aa4b6560e32418b267
Tri Le <trile95.it@gmail.com>
2024-06-18 23:02:22 -0400

    update about info

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -26,23 +26,33 @@ function About() {
         the first web project -
         <span className="font-medium italic">Smart Sensing Data Analysis</span>{" "}
         - I decided to pursue my passion in{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem and visualizing
-        the solution on the screen. My core stack is{" "}
+        <span className="font-medium">full-stack web development</span>.
+      </p>
+      <p className="mb-3">
+        <span className="italic">My favorite part of programming</span>{" "}
+        is the problem-solving aspect. I <span className="underline">love</span>{" "}
+        the feeling of finally figuring out a solution to a problem and seeing it
+        come to life on the screen.
+      </p>
+      <p className="mb-3">
+        My core stack is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          React, Next.js, Node, MySQL, MongoDB, and AWS
         </span>
-        . I am also familiar with Javascript and TypeScript. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a full-stack
-        developer.
+        . I am also <span className="underline">familiar</span> with{" "}
+        <span className="font-medium italic">
+          Agile Method, Python, and TypeScript
+        </span>.
+      </p>
+      <p className="mb-3">
+        I am always looking to learn new technologies and am currently seeking a{" "}
+        <span className="underline">full-time</span>{" "}
+        position as a <span className="font-medium italic">full-stack
+        developer</span>.
       </p>
       <p className="hidden">
         <span className="italic">When I'm not coding</span>, I enjoy watching Youtube to{" "}
-        <span className="font-medium">learn new things</span>. I am currently
-                                                           learning about{" "}
+        <span className="font-medium">learn new things</span>. I am currently learning about{" "}
         <span className="font-medium">being a part-time Youtuber</span>.
       </p>
     </motion.section>
